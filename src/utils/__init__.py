30b0912fc5ce087562a265be61740356bd7b89e9
harmandeep2993 <harmandeep2993@gmail.com>
2026-04-29 13:14:37 +0200

    feat(utils): add config loader, logger and data loader

---
+++ src/utils/__init__.py
@@ -0,0 +1,4 @@
+from .config import config, load_config
+from .logger import get_logger
+
+__all__ = ["config", "load_config", "get_logger"]
--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,3 +1,5 @@
+#src/utils/logger.py
+
 import logging
 import os
 
